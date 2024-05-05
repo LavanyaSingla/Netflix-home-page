a631c41a15db3fa5c2689e205b8144eaaa5d9396
LavanyaSingla <lavanyasingla322@gmail.com>
2024-05-05 12:09:10 +0530

    Add Banner page and genre list

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,29 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.scss';
 import axios from 'axios';
-const apiKey ="7097b47d67b4f154140d7023e8806ce"
-const url="https://api.themoviedb.org/3"
-const Card = (img) => (
+import { Link } from 'react-router-dom';
+
+const apiKey = "7097b47d67b4f154140d702d3e8806ce";
+const url = "https://api.themoviedb.org/3";
+const upcoming = "upcoming";
+const imgUrl = "https://image.tmdb.org/t/p/original"
+const now_playing = "now_playing";
+const popular = "popular";
+const top_rated = "top_rated";
+
+const Card = ({ img }) => (
     <img className='card' src={img} alt="cover" />
-)
-const Row = ({ title ,arr=[]}) => {
-    <div className='row'>
-        <h2>{title}</h2>
-        <div>
-            {
-                arr.map(()=>(
-                    <Card />
-                ))
-            }
+);
+const Row = ({ title, arr = [] }) => {
+    return (
+        <div className='row'>
+            <h2>{title}</h2>
+            <div>
+                {
+                    arr.map((item, index) => (
+                        <Card key={index} img={`${imgUrl}/${item.poster_path}`} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
+    )
 }
 const Home = () => {
+    const [upcomingMovies, setUpcomingMovies] = useState([])
+    const [playingMovies, setPlayingMovies] = useState([])
+    const [topRatedMovies, setTopRatedMovies] = useState([])
+    const [popularMovies, setPopularMovies] = useState([])
+    const [genre, setGenre] = useState([])
+    useEffect(() => {
+        const fetchData = async (type, setting) => {
+            const { data: { results } } = await axios.get(`${url}/movie/${type}?api_key=${apiKey}`);
+            setting(results)
+        }
+        fetchData(upcoming, setUpcomingMovies);
+        fetchData(now_playing, setPlayingMovies);
+        fetchData(top_rated, setTopRatedMovies);
+        fetchData(popular, setPopularMovies);
+
+        const fetchGenre = async () => {
+            const { data } = await axios.get(`${url}/genre/tv/list?api_key=${apiKey}`);
+
+            setGenre(data.genres);
+            console.log(data)
+        };
+        fetchGenre()
+    }, [])
+
     return (
         <section className="home">
             <div className="banner">
-                <Row title={"Popular on Netflix"} />
+
+                <Row title={"Upcoming"} arr={upcomingMovies} />
+                <Row title={"Recently Played"} arr={playingMovies} />
+                <Row title={"Top-Rated"} arr={topRatedMovies} />
+                <Row title={"Popular"} arr={popularMovies} />
+
+                <div className="genreBox">
+
+                    {
+                        genre.map((item) => (
+                            <Link key={item.id} to={`/genre/${item.id}`}>
+                                {item.name}
+                            </Link>
+                        ))
+                    }
+
+
+                </div>
             </div>
+
         </section>
     )
 }
